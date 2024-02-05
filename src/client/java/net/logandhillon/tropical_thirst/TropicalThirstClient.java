0da4b04820a1f79b1d328f5e9c5e1874dd530944
Logan Dhillon <logandhi64@gmail.com>
2024-02-04 22:24:28 -0500

    Initial commit

---
+++ src/client/java/net/logandhillon/tropical_thirst/TropicalThirstClient.java
@@ -0,0 +1,10 @@
+package net.logandhillon.tropical_thirst;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TropicalThirstClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/net/logandhillon/tropical_thirst/datagen/TropicalThirstDataGenerator.java
@@ -0,0 +1,11 @@
+package net.logandhillon.tropical_thirst.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TropicalThirstDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
